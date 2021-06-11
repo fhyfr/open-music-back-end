bcd709efb525727d500419cafa4d3d6db017bdff
Firman <fhaya.firman@gmail.com>
2021-06-11 09:57:01 +0700

    create routes

---
+++ src/api/songs/index.js
@@ -0,0 +1,11 @@
+const routes = require("./routes")
+
+
+module.exports = {
+  name: 'songs',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    
+    server.route(routes());
+  },
+}
